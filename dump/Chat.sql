bf2671d43ff2ef08c25eae30e32d5b136e10230d
Rost <myEmail@example.com>
2018-09-05 12:04:43 +0300

    Dump2

--- dump/Chat.sql
+++ dump/Chat.sql
@@ -16,6 +16,59 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role` (
+  `role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`role_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'ROLE_USER'),(2,'ROLE_ADMIN');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role_user`
+--
+
+DROP TABLE IF EXISTS `role_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `User_idx` (`user_id`),
+  KEY `Role_idx` (`role_id`),
+  CONSTRAINT `role_id` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`),
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role_user`
+--
+
+LOCK TABLES `role_user` WRITE;
+/*!40000 ALTER TABLE `role_user` DISABLE KEYS */;
+INSERT INTO `role_user` VALUES (1,1,1),(2,10,1),(3,11,1),(4,12,1),(5,13,1),(6,14,1),(7,15,1),(8,16,1),(9,17,1),(10,18,1);
+/*!40000 ALTER TABLE `role_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user`
 --
 
@@ -39,6 +92,14 @@ LOCK TABLES `user` WRITE;
 INSERT INTO `user` VALUES (1,'rost','123'),(3,'124','15124321421'),(4,'ghgfhf','hgfhgf'),(5,'ewqrew','rewrewrwer'),(6,'12','123'),(7,'1','1'),(8,'325235','123421'),(9,'324','123'),(10,'6546','65464'),(11,'123215421','1242141'),(12,'vbvb12','3213213'),(13,'123','gfgfg'),(14,'125215','$2a$10$HdsiIGjsnMBnjQ0jUn0YvOxsn6erMk4HveEmCZPoSrFnY3.zjTmBa'),(15,'2','$2a$10$LmJiqdh67PC9jSSPDq7AT.H6o8hlpc1nMSM3AcsNik6tLvBO2nXPG'),(16,'3','$2a$10$5au402RWe1MKU9X5lOOF8elKHAUsE4faSAC0X43H4z9rwYqaW9viS'),(17,'1234','$2a$10$4Y1O5clWQDjnPHwqIWDENuEq0cdDtc68IiMexMQQglzHAyyXvonna'),(18,'wow','$2a$10$H55hwABwHY1.jEhdeCoOvOZKLcAvZ1EPkgOo.FXRWouLFzeL2.Hj2');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping events for database 'chat'
+--
+
+--
+-- Dumping routines for database 'chat'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -49,4 +110,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-09-05 11:13:29
+-- Dump completed on 2018-09-05 12:02:29
